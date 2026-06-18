43ebb6adcee28eacb57c431f5263c0e22de11ac9
Edira Ochirova <eduuura@github.com>
2026-06-18 15:06:34 +0500

    fix: add LogoutView import in urls.py

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView  # ← ДОБАВЛЕНО
 from . import views
 
 app_name = 'main'
@@ -7,6 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('diary/', views.diary, name='diary'),
     path('add/', views.add_record, name='add_record'),
     path('edit/<int:pk>/', views.edit_record, name='edit_record'),
